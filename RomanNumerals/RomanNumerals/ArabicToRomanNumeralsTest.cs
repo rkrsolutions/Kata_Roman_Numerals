e8f72a5615c9e64a5557ae983d1f923d8a574b6d
RossRomanowski <sacratees@msn.com>
2016-11-06 19:55:33 -0500

    Initial check-in

---
+++ RomanNumerals/RomanNumerals/ArabicToRomanNumeralsTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumerals
+{
+    [TestClass]
+    public class ArabicToRomanNumeralsTest
+    {
+        [TestMethod]
+        public void NotNullTest()
+        {
+            Assert.IsNotNull(RomanNumeralConverter.ArabicToRomanNumerals(1));
+        }
+    }
+}
---
+++ RomanNumerals/RomanNumerals/RomanNumeralConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    public class RomanNumeralConverter
+    {
+        public static string ArabicToRomanNumerals(int arabicNumber)
+        {
+            return null;
+        }
+    }
+}
