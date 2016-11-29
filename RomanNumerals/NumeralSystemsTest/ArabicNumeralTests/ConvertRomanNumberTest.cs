9aad52c60b1221455608f10db0fa8c8b63372baa
RossRomanowski <sacratees@msn.com>
2016-11-29 13:51:10 -0500

    Added ConvertRomanNumber SingleNumeralToArabicNumberTest

--- RomanNumerals/NumeralSystemsTest/ArabicNumeralTests/ConvertRomanNumberTest.cs
+++ RomanNumerals/NumeralSystemsTest/ArabicNumeralTests/ConvertRomanNumberTest.cs
@@ -13,6 +13,8 @@ namespace NumeralSystemsTest.ArabicNumeralTests
     [TestClass]
     public class ConvertRomanNumberTest
     {
+        #region Methods
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void RomanNumberParameterNullTest()
@@ -32,5 +34,20 @@ namespace NumeralSystemsTest.ArabicNumeralTests
         {
             Assert.IsNotNull(ArabicNumeral.ConvertRomanNumber("ab"));
         }
+
+        [TestMethod]
+        public void SingleNumeralToArabicNumberTest()
+        {
+            Assert.IsTrue("1".IgnoreCaseEquals(ArabicNumeral.ConvertRomanNumber("i")));
+            Assert.IsTrue("5".IgnoreCaseEquals(ArabicNumeral.ConvertRomanNumber("v")));
+            Assert.IsTrue("10".IgnoreCaseEquals(ArabicNumeral.ConvertRomanNumber("x")));
+            Assert.IsTrue("50".IgnoreCaseEquals(ArabicNumeral.ConvertRomanNumber("l")));
+            Assert.IsTrue("100".IgnoreCaseEquals(ArabicNumeral.ConvertRomanNumber("c")));
+            Assert.IsTrue("500".IgnoreCaseEquals(ArabicNumeral.ConvertRomanNumber("d")));
+            Assert.IsTrue("1000".IgnoreCaseEquals(ArabicNumeral.ConvertRomanNumber("m")));
+        }
+
+        #endregion
+
     }
 }
